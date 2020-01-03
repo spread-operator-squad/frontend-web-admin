ba14825ef4a510041cb8d80bce3f911642652ad2
Franciska Deandra <franciska.karundeng@gmail.com>
2020-01-04 00:39:04 +0700

    [FEATURE] Able to fetch Item

---
+++ src/pages/Item/ItemContainer.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Table, Divider, Tag, message } from 'antd';
+import { fetchItem } from '../../services/itemService';
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: text => <a>{text}</a>,
+    },
+    {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+    },
+    {
+        title: 'Action',
+        key: 'action',
+        render: (text, record) => (
+            <span>
+                <a>Edit</a>
+                <Divider type="vertical" />
+                <a>Delete</a>
+            </span>
+        ),
+    },
+];
+
+class ItemContainer extends React.Component {
+    state = {
+        data: []
+    }
+
+    componentDidMount(){
+        this.fetchAllItems();
+    }
+
+    render() {
+        return (
+            <Table columns={columns} dataSource={this.state.data} />
+        );
+    }
+
+    fetchAllItems= async () =>{
+        const response = await fetchItem();
+        if (response.type === 'error') return message.warning(response.message);
+        console.log("GET RESPONSE", response);
+        this.setState({...this.state.data, data: response});
+    }
+}
+
+export default ItemContainer;
